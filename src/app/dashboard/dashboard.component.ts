02ba5b5fba1e1c18ea98aa687705b03c83b7d90f
ranjithkumar <mail2ranjithp@gmail.com>
2020-07-08 17:44:52 +0530

    email validation and email field

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,16 +15,20 @@ export class DashboardComponent implements OnInit {
   public filter:string = '';
   name: string;
   address : string;
-  dataSource: Array<{name: string, address: string}> = []
+  dataSource: Array<{name: string, email: string, address: string}> = []
   public list:any[] = productList.products;
 
   angForm: FormGroup;
    constructor(private fb: FormBuilder) {
     this.createForm();
   }
+
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+
    createForm() {
     this.angForm = this.fb.group({
        name: ['', Validators.required ],
+       email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
        address: ['', Validators.required ]
     });
   }
@@ -35,7 +39,7 @@ export class DashboardComponent implements OnInit {
   onSubmit() {
     if(this.angForm.valid){
       
-      this.dataSource.push({name: this.angForm.value.name, address: this.angForm.value.address });
+      this.dataSource.push({name: this.angForm.value.name, email: this.angForm.value.email, address: this.angForm.value.address });
       //console.log("form submitted", this.dataSource);
     }
     
