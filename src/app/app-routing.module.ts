2d5bbbb2095998a766085b3d93db1e5301625595
ranjithkumar <mail2ranjithp@gmail.com>
2020-07-08 13:08:21 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TableDetailComponent } from './table-detail/table-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'tableDetail', component: TableDetailComponent },
-  { path: '', redirectTo: '/tableDetail', pathMatch: 'full' }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
 ];
 
 @NgModule({
