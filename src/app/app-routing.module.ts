690f5f41b10211b12ec08e15ea2d8fc8a2d3350c
ranjithkumar <mail2ranjithp@gmail.com>
2020-07-08 09:44:57 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { TableDetailComponent } from './table-detail/table-detail.component';
 
-const routes: Routes = [{
-  path: 'tableDetail',
-  component: TableDetailComponent,
-},
-{path: '', redirectTo: 'tableDetail', pathMatch: 'full'}];
+const routes: Routes = [
+  { path: 'tableDetail', component: TableDetailComponent },
+  { path: '', redirectTo: '/tableDetail', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
