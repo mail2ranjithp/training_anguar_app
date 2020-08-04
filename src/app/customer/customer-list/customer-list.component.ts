000ea340478b5398bd90de123366c7657ad9a64b
ranjithkumar <mail2ranjithp@gmail.com>
2020-08-04 18:31:50 +0530

    added the filter for customer table

--- src/app/customer/customer-list/customer-list.component.ts
+++ src/app/customer/customer-list/customer-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-customer-list',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-list.component.sass']
 })
 export class CustomerListComponent implements OnInit {
+
+  searchString: string;
 
   constructor() { }
 
--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule }   from '@angular/forms';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
 
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   imports: [
     CommonModule,
-    CustomerRoutingModule
+    CustomerRoutingModule,
+    FormsModule 
   ],
-  declarations: [CustomerComponent, CustomerListComponent, CustomerDetailComponent]
+  declarations: [
+    CustomerComponent, 
+    CustomerListComponent, 
+    CustomerDetailComponent,
+    FilterPipe
+  ],
+  exports: [
+    // ...
+    FilterPipe
+]
 })
 export class CustomerModule { }
